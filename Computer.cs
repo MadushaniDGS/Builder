4f1cf5d4fe3a0ad61ed3359be606e92bfd20232f
Madushani <Madushani@DESKTOP-JV13050>
2024-08-26 16:37:07 +0530

    Add project files.

---
+++ Computer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+        public class Computer
+        {
+
+            private string cpu { get; set; }
+            private string ram { get; set; }
+            private string storage { get; set; }
+
+            Computer(ComputerBuilder computerBuilder)
+            {
+                this.cpu = computerBuilder.cpu;
+                this.ram = computerBuilder.ram;
+                this.storage = computerBuilder.storage;
+            }
+
+            public void Display()
+            {
+                Console.WriteLine($"{cpu} {ram} {storage}");
+            }
+
+            public class ComputerBuilder
+            {
+
+                public string cpu { get; set; }
+                public string ram { get; set; }
+                public string storage { get; set; }
+
+                public ComputerBuilder Cpu(string cpu)
+                {
+                    this.cpu = cpu;
+                    return this;
+                }
+
+                public ComputerBuilder Ram(String ram)
+                {
+                    this.ram = ram;
+                    return this;
+                }
+
+                public ComputerBuilder Storage(string storage)
+                {
+                    this.storage = storage;
+                    return this;
+                }
+
+                public Computer Build()
+                {
+                    return new Computer(this);
+                }
+
+            }
+
+        }
+    
+
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Builder;
+
+namespace designPatterns
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Computer c1 = new Computer.ComputerBuilder().
+            Cpu("I7").Ram("4GB").Storage("1TB").Build();
+            c1.Display();
+
+            Computer c2 = new Computer.ComputerBuilder().
+            Cpu("I3").Ram("3GB").Storage("16GB").Build();
+            c2.Display();
+
+        }
+    }
+}
